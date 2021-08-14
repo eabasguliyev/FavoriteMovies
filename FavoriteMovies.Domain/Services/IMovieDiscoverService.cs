346b48f965e51b7f94cd5b37aef6a96c06525095
eabasquliyev <elgun.abasquliyev2@gmail.com>
2021-08-14 15:10:58 +0400

    Create IMovieDiscoverService

---
+++ FavoriteMovies.Domain/Services/IMovieDiscoverService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using FavoriteMovies.Domain.Services.Results;
+
+namespace FavoriteMovies.Domain.Services
+{
+    public interface IMovieDiscoverService
+    {
+        Task<MovieDetailResult> GetMovieAsync(string imdbId);
+    }
+}
